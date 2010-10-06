94e7fb6fbcb8db180f6910b5f5aa66cf4e22f0fc
Barbie <barbie@missbarbell.co.uk>
2010-10-06 12:13:41 +0000

    Import of BARBIE/WWW-Scraper-ISBN-Yahoo_Driver-0.15 from CPAN.
    gitpan-cpan-distribution: WWW-Scraper-ISBN-Yahoo_Driver
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         BARBIE/WWW-Scraper-ISBN-Yahoo_Driver-0.15.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/WWW/Scraper/ISBN/Yahoo_Driver.pm
+++ lib/WWW/Scraper/ISBN/Yahoo_Driver.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.14';
+$VERSION = '0.15';
 
 #--------------------------------------------------------------------------
 
@@ -52,8 +52,8 @@ use constant    SEARCH  => 'http://search.shopping.yahoo.com/search?p=';
 
 =item C<search()>
 
-Creates a query string, then passes the appropriate form fields to the Yahoo
-Books server.
+Creates a query string, then passes the appropriate form fields to the 
+Yahoo Books server.
 
 The returned page should be the correct catalog page for that ISBN. If not the
 function returns zero and allows the next driver in the chain to have a go. If
